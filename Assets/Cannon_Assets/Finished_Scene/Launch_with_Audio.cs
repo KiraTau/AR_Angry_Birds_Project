acb45d293ebd49fd2a37606bce33c125f755736d
Cindy Huang <cindy-huang@hotmail.com>
2021-05-02 03:19:22 -0400

    ar angry birds

--- Assets/Cannon_Assets/Finished_Scene/Launch_with_Audio.cs
+++ Assets/Cannon_Assets/Finished_Scene/Launch_with_Audio.cs
@@ -1,74 +1,13 @@
-﻿using System.Collections;
+﻿// Launch_with_Audio with input from phone's touch screen.
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// using System;
-// using System;
 using sys = System;
 using TMPro;
 
 public class Launch_with_Audio : MonoBehaviour
 {
-
-
-    // public GameObject projectile;
-    // public float launchVelocity = 7f;
-    // public AudioClip shootsound;
-    // public float lowVolumeRange = .5f;
-    // public float highVolumeRange = 1.0f;
-    // private AudioSource source;
-    // private int count = 0;
-
-
-    // private UnityEngine.Vector3 firstPosition;
-
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-    //     source = GetComponent<AudioSource>();
-    // }
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     if (Input.touchCount > 0)
-    //     {
-    //         Touch touch = Input.GetTouch(0);
-
-    //         UnityEngine.Vector2 distVect = touch.deltaPosition;
-    //         float dist = distVect.magnitude;
-
-    //         // Debug.Log("Input");
-    //         // Debug.Log(Input.Touch);
-    //         // UnityEngine.Vector3 secondPosition = Input.Touch;
-    //         // Debug.Log("Diff");
-    //         // // Debug.Log(firstPosition - secondPosition);
-
-    //         // float dist = Vector3.Distance(firstPosition,secondPosition);
-    //         dist = dist / 10;
-    //         Debug.Log(dist);
-    //         // launchVelocity = launchVelocity * dist;
-    //         // sys.DateTime start = sys.DateTime.UtcNow;
-    //         // while (! Input.GetButtonUp("Fire1")) {
-    //         //     Debug.Log("waiting for up button");
-    //         //     // do nothing
-    //         // }
-    //         // sys.DateTime end = sys.DateTime.UtcNow;
-    //         // sys.TimeSpan timeDiff = end - start;
-    //         // double factor = sys.Convert.ToInt32(timeDiff.TotalMilliseconds);
-    //         // factor = factor / 1000;
-    //         // Debug.Log(factor);
-
-    //         float vol = Random.Range(lowVolumeRange, highVolumeRange);
-    //         source.PlayOneShot(shootsound, vol);
-
-    //         GameObject launchThis = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
-    //         launchThis.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, launchVelocity * dist, 0));
-
-    //         // launchVelocity = 7f;
-    //     }
-    // }
-
-//---------------------------------------------------- cellphone 2
     public GameObject projectile;
     public float launchVelocity = 2f;
     public AudioClip shootsound;
@@ -90,7 +29,7 @@ public class Launch_with_Audio : MonoBehaviour
 
     private UnityEngine.Vector3 firstPosition;
 
-    // Start is called before the first frame update
+    // Start is called before the first frame update.
     void Start()
     {
         source = GetComponent<AudioSource>();
@@ -100,6 +39,7 @@ public class Launch_with_Audio : MonoBehaviour
         lastPigText.SetActive(false);
     }
 
+    // Display text to show player's turn.
     void DisplayTurn(int turnCount) {
         if (turnCount % 2 == 0) {
             turnText.text = "Player 1's Turn!";
@@ -108,174 +48,51 @@ public class Launch_with_Audio : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+    // Update is called once per frame. Registers touch, instantiates, and launches projectile.
+    // If only one pig is left, sets doublePoints variable to true. If no pigs are left, 
+    // displays game over text.
     void Update()
     {
         if (Input.touchCount > 0) {
+            // Get furthest distance of swipe
             prevHasInput = true;
-
             Touch touch = Input.GetTouch(0);
             UnityEngine.Vector2 distVect = touch.deltaPosition;
             float dist = distVect.magnitude;
             dist = dist / 10;
             distance = dist;
-            // Debug.Log(dist);
 
-            // // Debug.Log("Input");
-            // // Debug.Log(Input.mousePosition);
-            // // Debug.Log("Diff");
-            // // Debug.Log(firstPosition - secondPosition);
-
-            // Touch touch = Input.GetTouch(0);
-            // UnityEngine.Vector2 distVect = touch.deltaPosition;
-            // float dist = distVect.magnitude;
-            // dist = dist / 10;
-            // Debug.Log(dist);
-            // // launchVelocity = launchVelocity * dist;
-            // // sys.DateTime start = sys.DateTime.UtcNow;
-            // // while (! Input.GetButtonUp("Fire1")) {
-            // //     Debug.Log("waiting for up button");
-            // //     // do nothing
-            // // }
-            // // sys.DateTime end = sys.DateTime.UtcNow;
-            // // sys.TimeSpan timeDiff = end - start;
-            // // double factor = sys.Convert.ToInt32(timeDiff.TotalMilliseconds);
-            // // factor = factor / 1000;
-            // // Debug.Log(factor);
-
-            // float vol = Random.Range(lowVolumeRange, highVolumeRange);
-            // source.PlayOneShot(shootsound, vol);
-
-            // GameObject launchThis = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
-            // launchThis.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, launchVelocity * dist, 0));
-
-            // // launchVelocity = 7f;
-            // turnCount = turnCount + 1;
-            // Debug.Log("turnCount");
-            // Debug.Log(turnCount);
-
-            // DisplayTurn(turnCount);
         } else {
             if (prevHasInput)
             {
                 float vol = Random.Range(lowVolumeRange, highVolumeRange);
                 source.PlayOneShot(shootsound, vol);
 
+                // Instantiate and launch projectile object
                 GameObject launchThis = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
                 launchThis.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, launchVelocity * distance, 0));
 
-                // launchVelocity = 7f;
-                turnCount = turnCount + 1;
-                Debug.Log("turnCount");
-                Debug.Log(turnCount);
+                turnCount = turnCount + 1; // Increment turn count
 
-                DisplayTurn(turnCount);            
+                DisplayTurn(turnCount); // Display player's turn
             }        
                 
             prevHasInput = false;
         }
 
+        // Actions that depend on total pigs left in game
         int totalPigs = GameObject.FindGameObjectsWithTag("Pig").Length;
         if (totalPigs == 1) {
-            // gameOverText.text = "GAME OVER";
             lastPigText.SetActive(true);
 
             doublePoints = true;
         }        
         else if (totalPigs == 0) {
-            // gameOverText.text = "GAME OVER";
-            // turnCount = turnCount + 1;
-            // DisplayTurn(turnCount, true);
-
+            // Display text
             lastPigText.SetActive(false);
             gameOverText.SetActive(true);
         }
     }
 
-
-
-
-// //----------------------------------------------------- computer
-    // public GameObject projectile;
-    // public float launchVelocity = 7f;
-    // public AudioClip shootsound;
-    // public float lowVolumeRange = .5f;
-    // public float highVolumeRange = 1.0f;
-    // private AudioSource source;
-    // public int turnCount;
-
-    // public TextMeshProUGUI turnText;
-    // public GameObject gameOverText;    
-
-
-    // private UnityEngine.Vector3 firstPosition;
-
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-    //     source = GetComponent<AudioSource>();
-    //     turnCount = 0;
-    //     DisplayTurn(turnCount);
-    //     gameOverText.SetActive(false);
-    // }
-
-    // void DisplayTurn(int turnCount) {
-    //     if (turnCount % 2 == 0) {
-    //         turnText.text = "Player 1's Turn!";
-    //     } else {
-    //         turnText.text = "Player 2's Turn!";
-    //     }
-    // }
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     if (Input.GetButtonDown("Fire1")) {
-    //         // Debug.Log("got down");
-    //         firstPosition = Input.mousePosition;
-    //     }
-    //     if (Input.GetButtonUp("Fire1"))
-    //     {
-    //         // Debug.Log("Input");
-    //         // Debug.Log(Input.mousePosition);
-    //         UnityEngine.Vector3 secondPosition = Input.mousePosition;
-    //         // Debug.Log("Diff");
-    //         // Debug.Log(firstPosition - secondPosition);
-
-    //         float dist = Vector3.Distance(firstPosition,secondPosition);
-    //         dist = dist / 10;
-    //         Debug.Log(dist);
-    //         // launchVelocity = launchVelocity * dist;
-    //         // sys.DateTime start = sys.DateTime.UtcNow;
-    //         // while (! Input.GetButtonUp("Fire1")) {
-    //         //     Debug.Log("waiting for up button");
-    //         //     // do nothing
-    //         // }
-    //         // sys.DateTime end = sys.DateTime.UtcNow;
-    //         // sys.TimeSpan timeDiff = end - start;
-    //         // double factor = sys.Convert.ToInt32(timeDiff.TotalMilliseconds);
-    //         // factor = factor / 1000;
-    //         // Debug.Log(factor);
-
-    //         float vol = Random.Range(lowVolumeRange, highVolumeRange);
-    //         source.PlayOneShot(shootsound, vol);
-
-    //         GameObject launchThis = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
-    //         launchThis.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, launchVelocity * dist, 0));
-
-    //         // launchVelocity = 7f;
-    //         turnCount = turnCount + 1;
-    //         Debug.Log("turnCount");
-    //         Debug.Log(turnCount);
-
-    //         DisplayTurn(turnCount);
-    //     }
-
-    //     int totalPigs = GameObject.FindGameObjectsWithTag("Pig").Length;
-    //     if (totalPigs == 0) {
-    //         // gameOverText.text = "GAME OVER";
-    //         gameOverText.SetActive(true);
-    //     }
-    // }
 
 }
---
+++ Assets/Cannon_Assets/Finished_Scene/Launch_with_Audio_computertesting.cs
@@ -0,0 +1,82 @@
+// Launch_with_Audio with input from computer mouse click, for debugging purposes.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using sys = System;
+using TMPro;
+
+public class Launch_with_Audio : MonoBehaviour
+{
+    public GameObject projectile;
+    public float launchVelocity = 7f;
+    public AudioClip shootsound;
+    public float lowVolumeRange = .5f;
+    public float highVolumeRange = 1.0f;
+    private AudioSource source;
+    public int turnCount;
+
+    public TextMeshProUGUI turnText;
+    public GameObject gameOverText;    
+
+
+    private UnityEngine.Vector3 firstPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        turnCount = 0;
+        DisplayTurn(turnCount);
+        gameOverText.SetActive(false);
+    }
+
+    void DisplayTurn(int turnCount) {
+        if (turnCount % 2 == 0) {
+            turnText.text = "Player 1's Turn!";
+        } else {
+            turnText.text = "Player 2's Turn!";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            // Debug.Log("got down");
+            firstPosition = Input.mousePosition;
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            // Debug.Log("Input");
+            // Debug.Log(Input.mousePosition);
+            UnityEngine.Vector3 secondPosition = Input.mousePosition;
+            // Debug.Log("Diff");
+            // Debug.Log(firstPosition - secondPosition);
+
+            float dist = Vector3.Distance(firstPosition,secondPosition);
+            dist = dist / 10;
+            Debug.Log(dist);
+
+            float vol = Random.Range(lowVolumeRange, highVolumeRange);
+            source.PlayOneShot(shootsound, vol);
+
+            GameObject launchThis = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+            launchThis.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, launchVelocity * dist, 0));
+
+            // launchVelocity = 7f;
+            turnCount = turnCount + 1;
+            Debug.Log("turnCount");
+            Debug.Log(turnCount);
+
+            DisplayTurn(turnCount);
+        }
+
+        int totalPigs = GameObject.FindGameObjectsWithTag("Pig").Length;
+        if (totalPigs == 0) {
+            // gameOverText.text = "GAME OVER";
+            gameOverText.SetActive(true);
+        }
+    }
+
+}
